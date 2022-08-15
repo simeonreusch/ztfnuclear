6e8f00d152245943b038fc696e6397a24ae6d501
simeonreusch <simeon.reusch@desy.de>
2022-08-15 14:51:00 +0200

    first real code

--- ztfnuclear/io.py
+++ ztfnuclear/io.py
@@ -2,32 +2,127 @@
 # Author: Simeon Reusch (simeon.reusch@desy.de)
 # License: BSD-3-Clause
 
-import os, logging, re
+import os, logging, re, subprocess
+from typing import Optional, List, Dict
 
 import pandas as pd
 
 _SOURCEDIR = os.path.dirname(os.path.realpath(__file__))
 LOCALSOURCE = os.path.join(os.getenv("ZTFDATA"), "nuclear_sample")
-REMOTESOURCE = "https://syncandshare.desy.de/index.php/s/GHeGQYxgk5FeToY/download"
+LOCALSOURCE_dfs = os.path.join(LOCALSOURCE, "FINAL_SAMPLE", "data")
+LOCALSOURCE_metadata = os.path.join(LOCALSOURCE, "FINAL_SAMPLE", "metadata.csv")
+DOWNLOAD_URL = "https://syncandshare.desy.de/index.php/s/GHeGQYxgk5FeToY/download"
+
+logger = logging.getLogger(__name__)
 
 if not os.path.exists(LOCALSOURCE):
     os.makedirs(LOCALSOURCE)
 
 
-def is_ztf_name(ztfid: str) -> bool:
+def download_if_neccessary():
+    """
+    Check if the dataframes have been downloaded and do so if neccessary
+    """
+    if os.path.exists(LOCALSOURCE_dfs):
+        csvs = []
+        for name in os.listdir(LOCALSOURCE_dfs):
+            if name[-4:] == ".csv":
+                csvs.append(name)
+        number_of_files = len(csvs)
+        logger.info(f"{number_of_files} dataframes found.")
+
+    else:
+        logger.info("Dataframe directory is not present, proceed to download files.")
+        download()
+
+
+def download():
+    """
+    Downloads the dataframe from DESY Syncandshare
+    """
+    cmd = f"wget {DOWNLOAD_URL} -P {LOCALSOURCE}; mv {LOCALSOURCE}/download {LOCALSOURCE}/download.tar; tar -xvf {LOCALSOURCE}/download.tar -C {LOCALSOURCE}; rm {LOCALSOURCE}/download.tar"
+
+    subprocess.run(cmd, shell=True)
+    logger.info("Download complete")
+
+
+def get_all_ztfids() -> List[str]:
+    """
+    Checks the download folder and gets all ztfids
+    """
+    ztfids = []
+    for name in os.listdir(LOCALSOURCE_dfs):
+        if name[-4:] == ".csv":
+            ztfids.append(name[:-4])
+    return ztfids
+
+
+def is_valid_ztfid(ztfid: str) -> bool:
     """
     Checks if a string adheres to the ZTF naming scheme
     """
-    is_match = re.match("^ZTF[1-2]\d[a-z]{7}$", name)
+    is_match = re.match("^ZTF[1-2]\d[a-z]{7}$", ztfid)
     if is_match:
         return True
     else:
         return False
 
 
-def get_ztfid_dataframe(ztfid: str) -> pd.DataFrame:
+def get_metadata() -> pd.DataFrame:
+    """
+    Gets the metadata dataframe for the full sample
+    """
+    df = pd.read_csv(LOCALSOURCE_metadata, index_col=0)
+    return df
+
+
+def get_ztfid_dataframe(ztfid: str) -> Optional[pd.DataFrame]:
     """
     Get the Pandas Dataframe of a single transient
     """
-    filepath = os.path.join(LOCALSOURCE)
-    return df
+    if is_valid_ztfid(ztfid):
+        filepath = os.path.join(LOCALSOURCE_dfs, f"{ztfid}.csv")
+        try:
+            df = pd.read_csv(filepath, comment="#")
+            return df
+        except FileNotFoundError:
+            logger.warn(f"No file found for {ztfid}. Check the ID.")
+            return None
+    else:
+        raise ValueError(f"{ztfid} is not a valid ZTF ID")
+
+
+def get_ztfid_header(ztfid: str) -> Optional[dict]:
+    """
+    Returns the metadata contained in the csvs as dictionary
+    """
+    if is_valid_ztfid(ztfid):
+        filepath = os.path.join(LOCALSOURCE_dfs, f"{ztfid}.csv")
+
+        try:
+            with open(filepath, "r") as input_file:
+                headerlines = []
+                for i, line in enumerate(input_file):
+                    if i == 6:
+                        break
+                    line = line.split(",", 2)[0].split("=")[1][:-1]
+                    headerlines.append(line)
+                ztfid, ra, dec, lastobs, lastdownload, lastfit = headerlines
+
+                returndict = {
+                    "ztfid": ztfid,
+                    "ra": ra,
+                    "dec": dec,
+                    "lastobs": lastobs,
+                    "lastdownload": lastdownload,
+                    "lastfit": lastfit,
+                }
+
+                return returndict
+
+        except FileNotFoundError:
+            logger.warn(f"No file found for {ztfid}. Check the ID.")
+            return None
+
+    else:
+        raise ValueError(f"{ztfid} is not a valid ZTF ID")
--- ztfnuclear/sample.py
+++ ztfnuclear/sample.py
@@ -17,4 +17,22 @@ class NuclearSample(object):
     def __init__(self):
         super(NuclearSample, self).__init__()
         self.logger = logging.getLogger(__name__)
-        self.logger.info("LALALA")
+        self.logger.info("Initializing sample")
+
+        io.download_if_neccessary()
+
+        self.get_all_ztfids()
+        self.meta()
+
+    def get_transient(self, ztfid: str):
+        df = io.get_ztfid_dataframe(ztfid=ztfid)
+        header = io.get_ztfid_header(ztfid=ztfid)
+        return header, df
+
+    def get_all_ztfids(self):
+        all_ztfids = io.get_all_ztfids()
+        self.ztfids = all_ztfids
+
+    def meta(self):
+        df = io.get_metadata()
+        self.meta = df
