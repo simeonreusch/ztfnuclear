ad8d347a0cb9f2df1d9258a4007ced8f99337632
simeonreusch <simeon.reusch@desy.de>
2022-10-14 06:52:48 +0200

    made multiprocessed fitting prettier

--- fit.py
+++ fit.py
@@ -6,24 +6,37 @@ import pandas as pd
 import matplotlib
 from tqdm import tqdm
 
+from ztfnuclear.database import MetadataDB
 from ztfnuclear.sample import NuclearSample, Transient
 
 s = NuclearSample()
-nprocess = 6
+meta = MetadataDB()
+
+cores = multiprocessing.cpu_count()
+nprocess = int(cores / 2)
+
 
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
+FIT_TYPE = "tde_fit_exp"
+RECREATE_BASELINE = True
+
 
 def _tde_fitter(ztfid):
     t = Transient(ztfid)
-    t.recreate_baseline()
-    t.fit_tde()
-    # t.plot_tde()
-    return ztfid
+    if RECREATE_BASELINE:
+        t.recreate_baseline()
+    if FIT_TYPE == "tde_fit_exp":
+        t.fit_tde()
 
 
 if __name__ == "__main__":
+
+    logger = logging.getLogger(__name__)
+
+    logger.info(f"Running fits for {FIT_TYPE} in {nprocess} threads.")
+
     startindex = 0
 
     with multiprocessing.Pool(nprocess) as p:
@@ -32,3 +45,11 @@ if __name__ == "__main__":
             total=len(s.ztfids[startindex:]),
         ):
             a = 1
+
+    fitres = meta.read_parameters(["_id", FIT_TYPE])
+    outfile = f"{FIT_TYPE}.json"
+
+    logger.info(f"Fitting for {FIT_TYPE} done, exporting result to {outfile}")
+
+    with open(outfile, "w") as fp:
+        json.dump(fitres, fp)
