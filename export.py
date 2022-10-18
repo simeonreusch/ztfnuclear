e7a8d5e06eae428e3412963a3758d36fde904cbd
simeonreusch <simeon.reusch@desy.de>
2022-10-18 12:49:15 -0500

    make export.py read commandline arg

--- export.py
+++ export.py
@@ -2,16 +2,28 @@
 # Author: Simeon Reusch (simeon.reusch@desy.de)
 # License: BSD-3-Clause
 
+import os, logging, re, json, argparse
 from ztfnuclear.database import MetadataDB
-import os, logging, re, json
 
 meta = MetadataDB()
 
-params = ["comments", "rating"]
+if __name__ == "__main__":
 
-for p in params:
-    content = meta.read_parameters(["_id", p])
-    outfile = f"{p}.json"
+    logger = logging.getLogger(name=__name__)
+    parser = argparse.ArgumentParser(description="Export keys from metadata MongoDB")
+
+    parser.add_argument(
+        "key_name",
+        type=str,
+        help="Provide a key of the database",
+    )
+
+    commandline_args = parser.parse_args()
+
+    content = meta.read_parameters(["_id", commandline_args.key_name])
+    outfile = f"{commandline_args.key_name}.json"
 
     with open(outfile, "w") as fp:
         json.dump(content, fp)
+
+    logger.info(f"Dumped to {outfile}")
