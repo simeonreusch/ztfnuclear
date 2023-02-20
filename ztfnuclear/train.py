be4d5247a6ce7525b1d89d470f9ec44c72ce6425
simeonreusch <simeon.reusch@desy.de>
2023-02-20 17:12:50 +0100

    confusion matrix

--- ztfnuclear/train.py
+++ ztfnuclear/train.py
@@ -12,6 +12,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 from numpy.random import default_rng
+from matplotlib import pyplot as plt
 
 import xgboost as xgb
 from sklearn import metrics
@@ -46,7 +47,8 @@ class Model(object):
         train_test_fraction: float = 0.7,
         n_iter: int = 10,
         grid_search_sample_size: int = 1000,
-        model_dir: Path | str | None = None,
+        model_dir: Path | str = Path(io.MODEL_dir),
+        plot_dir: Path | str = Path(io.LOCALSOURCE_plots),
     ):
         super(Model, self).__init__()
         self.logger = logging.getLogger(__name__)
@@ -57,15 +59,21 @@ class Model(object):
         self.n_iter = n_iter
         self.grid_search_sample_size = grid_search_sample_size
 
-        if model_dir is None:
-            self.model_dir = io.MODEL_dir
-        elif isinstance(model, str):
+        self.le = LabelEncoder()
+
+        if isinstance(model_dir, str):
             self.model_dir = Path(model_dir)
         else:
             self.model_dir = model_dir
 
-        if not self.model_dir.is_dir():
-            os.makedirs(self.model_dir)
+        if isinstance(plot_dir, str):
+            self.plot_dir = Path(plot_dir)
+        else:
+            self.plot_dir = plot_dir
+
+        for p in [self.model_dir, self.plot_dir]:
+            if not p.is_dir():
+                os.makedirs(p)
 
         self.rng = default_rng(seed=self.seed)
         self.get_training_metadata()
@@ -97,7 +105,6 @@ class Model(object):
         self.meta = self.meta.astype({"median_distnr": "float64", "classif": "str"})
 
         self.logger.info(f"Read metadata. {len(self.meta)} transients available.")
-        self.meta.to_csv("test.csv")
         self.all_ztfids = self.meta.index.values
 
         self.parent_ztfids = self.get_parent_ztfids(self.all_ztfids)
@@ -121,7 +128,16 @@ class Model(object):
             for ztfid in self.parent_ztfids
             if ztfid not in self.validation_parent_ztfids
         ]
-        self.validation_sample = self.meta.query("index in @self.validation_ztfids")
+        df_validation = self.meta.query("index in @self.validation_ztfids")
+        df_validation = shuffle(df_validation, random_state=self.seed).reset_index(
+            drop=True
+        )
+        self.X_validation = df_validation.drop(columns="classif").reset_index(drop=True)
+        self.y_validation = df_validation.filter(["classif"]).reset_index(drop=True)
+        self.y_validation["classif"] = self.le.fit_transform(
+            self.y_validation["classif"]
+        )
+        self.y_validation = self.y_validation["classif"]
 
     def train_test_split(self):
         """
@@ -155,17 +171,15 @@ class Model(object):
         df_train = shuffle(df_train, random_state=self.seed).reset_index(drop=True)
         df_test = shuffle(df_test, random_state=self.seed).reset_index(drop=True)
 
-        le = LabelEncoder()
-
         self.X_train = df_train.drop(columns="classif").reset_index(drop=True)
         self.X_test = df_test.drop(columns="classif").reset_index(drop=True)
         self.y_train = df_train.filter(["classif"]).reset_index(drop=True)
 
-        self.y_train["classif"] = le.fit_transform(self.y_train["classif"])
+        self.y_train["classif"] = self.le.fit_transform(self.y_train["classif"])
         self.y_train = self.y_train["classif"]
 
         self.y_test = df_test.filter(["classif"]).reset_index(drop=True)
-        self.y_test["classif"] = le.fit_transform(self.y_test["classif"])
+        self.y_test["classif"] = self.le.fit_transform(self.y_test["classif"])
         self.y_test = self.y_test["classif"]
 
     def get_parent_ztfids(self, ztfids: List[str]):
@@ -286,7 +300,7 @@ class Model(object):
         # Load the stuff
         infile_grid = (
             self.model_dir
-            / f"grid_result_niter_{self.n_iter}_nsample_{self.grid_search_sample_size}",
+            / f"grid_result_niter_{self.n_iter}_nsample_{self.grid_search_sample_size}"
         )
 
         grid_result = joblib.load(infile_grid)
@@ -295,106 +309,158 @@ class Model(object):
         self.grid_result = grid_result
         self.best_estimator = best_estimator
 
+        # print(self.le.get_params())
+        # quit()
+
         self.logger.info(f"Loading best estimator. Parameters:\n{self.best_estimator}")
 
         # Plot feature importance for full set
+        self.logger.info("Plotting feature importance")
         self.plot_features()
 
-        """
-        Now we cut the test sample so that only one datapoint
-        per stock-ID survives
-        """
-        df_test_subsample = self.get_random_stock_subsample(self.df_test)
+        self.logger.info("Plotting evaluation")
+        self.logger.info(f"\nWe now plot the evaluation")
 
-        logger.info(f"Best: {grid_result.best_score_} using {grid_result.best_params_}")
+        features = self.X_validation
+        target = self.y_validation
 
-        self.df_test_subsample = df_test_subsample
+        pred = best_estimator.predict(features)
+        # print(target)
+        # print(pdred)
+        # quit()
+        # precision = metrics.precision_score(
+        #     target, pred, labels=[0, 1, 2, 3, 4], average="samples"
+        # )
+        # recall = metrics.recall_score(
+        #     target, pred, labels=[0, 1, 2, 3, 4], average="samples"
+        # )
+        # confusion = metrics.multilabel_confusion_matrix(
+        #     y_true=target, y_pred=pred, labels=[0, 1, 2, 3, 4]
+        # )
+        # disp = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion)
+        # 0: agn
+        # 1: sn_ia
+        # 2: sn_other
+        # 3: star
+        # 4: tde
 
-        # We get even sized binning (at least as far as possible)
-        evaluation_bins, nbins = self.get_optimal_bins(nbins=14)
-
-        self.evaluation_bins = evaluation_bins
-
-        logger.info(f"\nWe now plot the evaluation using {nbins} time bins")
-
-        precision_list = []
-        recall_list = []
-        aucpr_list = []
-        timebin_mean_list = []
-
-        for timebin in evaluation_bins:
-            df_test_bin = df_test_subsample[
-                (df_test_subsample["ndet"] >= timebin[0])
-                & (df_test_subsample["ndet"] <= timebin[1])
-            ]
-            X_test = df_test_bin.drop(columns=["class_short", "stock"])
-
-            self.cols_to_use.append("stock")
-            y_test = df_test_bin.drop(columns=self.cols_to_use)
-            features = X_test
-            target = y_test
-
-            pred = best_estimator.predict(features)
-
-            precision_list.append(metrics.precision_score(target, pred))
-            recall_list.append(metrics.recall_score(target, pred))
-            aucpr_list.append(metrics.average_precision_score(target, pred))
-
-            timebin_mean_list.append(np.mean([timebin[0], timebin[1]]))
-
-        outfiles = [
-            os.path.join(
-                self.plot_dir,
-                f"{i}_niter_{self.n_iter}_nsample_{self.grid_search_sample_size}.png",
-            )
-            for i in ["precision", "recall", "aucpr"]
-        ]
-
-        fig, ax = plt.subplots(figsize=(5, 5))
-        ax.scatter(timebin_mean_list, precision_list)
-        ax.set_xlabel("ndet interval center")
-        ax.set_ylabel("precision")
-        ax.set_ylim([0.5, 1])
-        fig.savefig(outfiles[0], dpi=300)
-        plt.close()
-
-        fig, ax = plt.subplots(figsize=(5, 5))
-        ax.scatter(timebin_mean_list, recall_list)
-        ax.set_xlabel("ndet interval center")
-        ax.set_ylabel("recall")
-        ax.set_ylim([0.75, 1])
-        fig.savefig(outfiles[1], dpi=300)
-        plt.close()
-
-        fig, ax = plt.subplots(figsize=(5, 5))
-        ax.scatter(timebin_mean_list, aucpr_list)
-        ax.set_xlabel("ndet interval center")
-        ax.set_ylabel("aucpr")
-        ax.set_ylim([0.5, 1])
-        fig.savefig(outfiles[2], dpi=300)
-        plt.close()
-
-    def get_optimal_bins(self, nbins=20):
-        """
-        Determine optimal time bins (requirement: same number
-        of alerts per bin). This cannot always be fulfilled, so duplicates=drop is passed.
-        """
-        out, bins = pd.qcut(
-            self.df_test_subsample.ndet.values, nbins, retbins=True, duplicates="drop"
+        disp = metrics.ConfusionMatrixDisplay.from_predictions(
+            y_true=target,
+            y_pred=pred,
+            display_labels=["agn", "snia", "sn_other", "star", "tde"],
+            normalize="true",
         )
-        final_bins = []
-        for i in range(len(bins) - 1):
-            final_bins.append([int(bins[i]), int(bins[i + 1])])
-        nbins = len(final_bins)
-        return final_bins, nbins
+        disp.plot()
+        outfile = self.plot_dir / f"results_seed_{self.seed}.pdf"
+        plt.savefig(outfile)
+        # print(confusion)
+        # aucpr = metrics.average_precision_score(target, pred, average="samples")
 
-    def get_random_stock_subsample(self, df):
-        """
-        Returns a df consisting of one random datapoint for each unique stock ID
-        """
-        df_sample = df.groupby("stock").sample(n=1, random_state=self.random_state)
+        # self.logger.info(
+        # f"Precision: {precision} / recall: {recall}"
+        # )  # / aucpr: {aucpr}")
 
-        return df_sample
+    #         target = y_test
+
+    #         pred = best_estimator.predict(features)
+
+    #         precision_list.append(metrics.precision_score(target, pred))
+    #         recall_list.append(metrics.recall_score(target, pred))
+    #         aucpr_list.append(metrics.average_precision_score(target, pred))
+
+    #         timebin_mean_list.append(np.mean([timebin[0], timebin[1]]))
+
+    #     df_test_subsample = self.get_random_stock_subsample(self.df_test)
+
+    #     logger.info(f"Best: {grid_result.best_score_} using {grid_result.best_params_}")
+
+    #     self.df_test_subsample = df_test_subsample
+
+    #     # We get even sized binning (at least as far as possible)
+    #     evaluation_bins, nbins = self.get_optimal_bins(nbins=14)
+
+    #     self.evaluation_bins = evaluation_bins
+
+    #     logger.info(f"\nWe now plot the evaluation using {nbins} time bins")
+
+    #     precision_list = []
+    #     recall_list = []
+    #     aucpr_list = []
+    #     timebin_mean_list = []
+
+    #     for timebin in evaluation_bins:
+    #         df_test_bin = df_test_subsample[
+    #             (df_test_subsample["ndet"] >= timebin[0])
+    #             & (df_test_subsample["ndet"] <= timebin[1])
+    #         ]
+    #         X_test = df_test_bin.drop(columns=["class_short", "stock"])
+
+    #         self.cols_to_use.append("stock")
+    #         y_test = df_test_bin.drop(columns=self.cols_to_use)
+    #         features = X_test
+    #         target = y_test
+
+    #         pred = best_estimator.predict(features)
+
+    #         precision_list.append(metrics.precision_score(target, pred))
+    #         recall_list.append(metrics.recall_score(target, pred))
+    #         aucpr_list.append(metrics.average_precision_score(target, pred))
+
+    #         timebin_mean_list.append(np.mean([timebin[0], timebin[1]]))
+
+    #     outfiles = [
+    #         os.path.join(
+    #             self.plot_dir,
+    #             f"{i}_niter_{self.n_iter}_nsample_{self.grid_search_sample_size}.png",
+    #         )
+    #         for i in ["precision", "recall", "aucpr"]
+    #     ]
+
+    #     fig, ax = plt.subplots(figsize=(5, 5))
+    #     ax.scatter(timebin_mean_list, precision_list)
+    #     ax.set_xlabel("ndet interval center")
+    #     ax.set_ylabel("precision")
+    #     ax.set_ylim([0.5, 1])
+    #     fig.savefig(outfiles[0], dpi=300)
+    #     plt.close()
+
+    #     fig, ax = plt.subplots(figsize=(5, 5))
+    #     ax.scatter(timebin_mean_list, recall_list)
+    #     ax.set_xlabel("ndet interval center")
+    #     ax.set_ylabel("recall")
+    #     ax.set_ylim([0.75, 1])
+    #     fig.savefig(outfiles[1], dpi=300)
+    #     plt.close()
+
+    #     fig, ax = plt.subplots(figsize=(5, 5))
+    #     ax.scatter(timebin_mean_list, aucpr_list)
+    #     ax.set_xlabel("ndet interval center")
+    #     ax.set_ylabel("aucpr")
+    #     ax.set_ylim([0.5, 1])
+    #     fig.savefig(outfiles[2], dpi=300)
+    #     plt.close()
+
+    # def get_optimal_bins(self, nbins=20):
+    #     """
+    #     Determine optimal time bins (requirement: same number
+    #     of alerts per bin). This cannot always be fulfilled, so duplicates=drop is passed.
+    #     """
+    #     out, bins = pd.qcut(
+    #         self.df_test_subsample.ndet.values, nbins, retbins=True, duplicates="drop"
+    #     )
+    #     final_bins = []
+    #     for i in range(len(bins) - 1):
+    #         final_bins.append([int(bins[i]), int(bins[i + 1])])
+    #     nbins = len(final_bins)
+    #     return final_bins, nbins
+
+    # def get_random_stock_subsample(self, df):
+    #     """
+    #     Returns a df consisting of one random datapoint for each unique stock ID
+    #     """
+    #     df_sample = df.groupby("stock").sample(n=1, random_state=self.random_state)
+
+    #     return df_sample
 
     def plot_features(self):
         """
@@ -403,17 +469,17 @@ class Model(object):
 
         fig, ax = plt.subplots(figsize=(10, 21))
 
-        cols = self.cols_to_use
+        cols = list(self.X_train.keys())
 
-        cols.remove("stock")
+        # cols.remove("classif")
 
         ax.barh(cols, self.best_estimator.feature_importances_)
         plt.title("Feature importance", fontsize=25)
         plt.tight_layout()
 
-        outfile = os.path.join(
-            self.plot_dir,
-            f"feature_importance_niter_{self.n_iter}_nsample_{self.grid_search_sample_size}.png",
+        outfile = (
+            self.plot_dir
+            / f"feature_importance_niter_{self.n_iter}_nsample_{self.grid_search_sample_size}.pdf"
         )
 
         fig.savefig(
